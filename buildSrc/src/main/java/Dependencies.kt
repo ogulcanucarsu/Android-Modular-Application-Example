a124c6e77b6424b2d8d1dbae5f58dc8622644220
Ogulcan <ogulcanucarsu@gmail.com>
2019-08-13 22:40:17 +0300

    added base data dependency

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -19,4 +19,13 @@ object HelperLibraries {
 
 object CoreLibraries {
     const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlinVersion}"
+    const val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutinesVersion}"
+    const val coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutinesVersion}"
+    const val dagger2AndroidSupport = "com.google.dagger:dagger-android-support:${Versions.dagger2Version}"
+    const val gson = "com.squareup.retrofit2:converter-gson:${Versions.gsonVersion}"
+    const val logInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okHttpLoggingInterceptorVersion}"
+    const val okHttp = "com.squareup.okhttp3:okhttp:${Versions.okHttpVersion}"
+    const val retrofitCoroutineAdapter =
+        "com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:${Versions.retrofitCoroutineAdapterVersion}"
+    const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofitVersion}"
 }
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -21,4 +21,13 @@ object Versions{
     //Core Lib. Version
     const val kotlinVersion = "1.3.21"
     const val gradleVersion = "3.4.0"
+    const val coroutinesVersion = "1.0.1"
+    const val dagger2Version = "2.22.1"
+    const val gsonVersion = "2.0.2"
+    const val retrofitVersion = "2.5.0"
+    const val okHttpLoggingInterceptorVersion = "3.8.0"
+    const val retrofitCoroutineAdapterVersion = "0.9.2"
+    const val okHttpVersion = "3.10.0"
+
+
 }
