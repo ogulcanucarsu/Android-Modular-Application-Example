776b4c6af50a02d9e47c31255b784562612f3115
Ogulcan <ogulcanucarsu@gmail.com>
2019-08-13 22:09:59 +0300

    base directory and base data,domain,presentation created

--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -3,4 +3,12 @@ object Modules {
     // App
     const val app = ":app"
 
+    // Directory
+    const val base = ":base"
+
+    //Base
+    const val baseData = "$base:base_data"
+    const val baseDomain = "$base:base_domain"
+    const val basePresentation = "$base:base_presentation"
+
 }
