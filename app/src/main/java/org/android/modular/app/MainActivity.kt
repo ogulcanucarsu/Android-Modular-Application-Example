a982341a0f8c2369d88d92474fa113fec7d52079
Ogulcan <ogulcanucarsu@gmail.com>
2019-08-14 19:18:13 +0300

    androidx bug fixed

--- app/src/main/java/org/android/modular/app/MainActivity.kt
+++ app/src/main/java/org/android/modular/app/MainActivity.kt
@@ -1,7 +1,7 @@
 package org.android.modular.app
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
