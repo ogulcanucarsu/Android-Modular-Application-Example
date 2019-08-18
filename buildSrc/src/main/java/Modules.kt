1944861a03470e1243cbcfdea41aec88283a1ece
Ogulcan <ogulcanucarsu@gmail.com>
2019-08-19 01:06:55 +0300

    added base navigation module

--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -10,5 +10,6 @@ object Modules {
     const val baseData = "$base:base_data"
     const val baseDomain = "$base:base_domain"
     const val basePresentation = "$base:base_presentation"
+    const val baseNavigation = "$base:base_navigation"
 
 }
